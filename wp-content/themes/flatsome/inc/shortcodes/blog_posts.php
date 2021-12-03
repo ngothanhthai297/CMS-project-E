f546cce421f374c4d506dae7f703965abe1caa98
ngo thanh thai <ngothai653@gmail.com>
2021-12-03 13:29:44 +0700

    Update module-3 latest news

--- wp-content/themes/flatsome/inc/shortcodes/blog_posts.php
+++ wp-content/themes/flatsome/inc/shortcodes/blog_posts.php
@@ -129,7 +129,7 @@ function shortcode_latest_from_blog($atts, $content = null, $tag = '' ) {
 	);
 
 	$css_image_height = array(
-      array( 'attribute' => 'padding-top', 'value' => $image_height),
+      array( 'attribute' => 'padding-top', 'value' => '105%'),
   	);
 
 	$css_args = array(
@@ -228,52 +228,61 @@ while ( $recentPosts->have_posts() ) : $recentPosts->the_post();
 	    }
 
 		?>
-		<div class="col <?php echo implode(' ', $col_class); ?>" <?php echo $animate;?>>
-			<div class="col-inner">
-			<a href="<?php the_permalink() ?>" class="plain">
-				<div class="box <?php echo $classes_box; ?> box-blog-post has-hover">
-          <?php if(has_post_thumbnail()) { ?>
-  					<div class="box-image" <?php echo get_shortcode_inline_css($css_args_img); ?>>
-  						<div class="<?php echo $classes_image; ?>" <?php echo get_shortcode_inline_css($css_image_height); ?>>
-  							<?php the_post_thumbnail($image_size); ?>
-  							<?php if($image_overlay){ ?><div class="overlay" style="background-color: <?php echo $image_overlay;?>"></div><?php } ?>
-  							<?php if($style == 'shade'){ ?><div class="shade"></div><?php } ?>
-  						</div>
-  						<?php if($post_icon && get_post_format()) { ?>
-  							<div class="absolute no-click x50 y50 md-x50 md-y50 lg-x50 lg-y50">
-  				            	<div class="overlay-icon">
-  				                    <i class="icon-play"></i>
-  				                </div>
-  				            </div>
-  						<?php } ?>
-  					</div>
-          <?php } ?>
-					<div class="box-text <?php echo $classes_text; ?>" <?php echo get_shortcode_inline_css($css_args); ?>>
-					<div class="box-text-inner blog-post-inner">
+<div class="col <?php echo implode(' ', $col_class); ?>" <?php echo $animate;?>>
+    <div class="col-inner">
+        <a href="<?php the_permalink() ?>" class="plain"> </a>
+        <div class="box <?php echo $classes_box; ?> box-blog-post has-hover">
+            <?php if(has_post_thumbnail()) { ?>
+            <div class="box-image" <?php echo get_shortcode_inline_css($css_args_img); ?>>
+                <div class="<?php echo $classes_image; ?>" <?php echo get_shortcode_inline_css($css_image_height); ?>>
+                    <?php the_post_thumbnail($image_size); ?>
+                    <?php if($image_overlay){ ?><div class="overlay"
+                        style="background-color: <?php echo $image_overlay;?>"></div><?php } ?>
+                    <?php if($style == 'shade'){ ?><div class="shade"></div><?php } ?>
+                </div>
+                <?php if($post_icon && get_post_format()) { ?>
+                <div class="absolute no-click x50 y50 md-x50 md-y50 lg-x50 lg-y50">
+                    <div class="overlay-icon">
+                        <i class="icon-play"></i>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+            <?php } ?>
+            <div class="box-shadow-readmore">
+                <div class="box-text <?php echo $classes_text; ?>" <?php echo get_shortcode_inline_css($css_args); ?>>
+                    <div class="box-text-inner blog-post-inner">
 
-					<?php do_action('flatsome_blog_post_before'); ?>
+                        <?php do_action('flatsome_blog_post_before'); ?>
 
-					<?php if($show_category !== 'false') { ?>
-						<p class="cat-label <?php if($show_category == 'label') echo 'tag-label'; ?> is-xxsmall op-7 uppercase">
-					<?php
+                        <?php if($show_category !== 'false') { ?>
+                        <p
+                            class="cat-label <?php if($show_category == 'label') echo 'tag-label'; ?> is-xxsmall op-7 uppercase">
+                            <?php
 						foreach((get_the_category()) as $cat) {
 						echo $cat->cat_name . ' ';
 					}
 					?>
-					</p>
-					<?php } ?>
-					<h5 class="post-title is-<?php echo $title_size; ?> <?php echo $title_style;?>"><?php the_title(); ?></h5>
-					<?php if((!has_post_thumbnail() && $show_date !== 'false') || $show_date == 'text') {?><div class="post-meta is-small op-8"><?php echo get_the_date(); ?></div><?php } ?>
-					<div class="is-divider"></div>
-					<?php if($show_excerpt !== 'false') { ?>
-					<p class="from_the_blog_excerpt <?php if($show_excerpt !== 'visible'){ echo 'show-on-hover hover-'.$show_excerpt; } ?>"><?php
+                        </p>
+                        <?php } ?>
+						<?php the_category(); ?>
+
+                        <h5 class="post-title is-<?php echo $title_size; ?> <?php echo $title_style;?>">
+                            <?php the_title(); ?></h5>
+
+                        <?php if((!has_post_thumbnail() && $show_date !== 'false') || $show_date == 'text') {?><div
+                            class="post-meta is-small op-8"><?php echo get_the_date(); ?></div><?php } ?>
+                        <!-- <div class="is-divider"></div> -->
+                        <?php if($show_excerpt !== 'false') { ?>
+                        <p
+                            class="from_the_blog_excerpt <?php if($show_excerpt !== 'visible'){ echo 'show-on-hover hover-'.$show_excerpt; } ?>"><?php
 					  $the_excerpt  = get_the_excerpt();
 					  $excerpt_more = apply_filters( 'excerpt_more', ' [...]' );
-					  echo flatsome_string_limit_words($the_excerpt, $excerpt_length) . $excerpt_more;
+					//   echo flatsome_string_limit_words($the_excerpt, $excerpt_length) . $excerpt_more;
 					?>
-					</p>
-					<?php } ?>
-                    <?php if ( $comments == 'true' && comments_open() && '0' != get_comments_number() ) { ?>
+                        </p>
+                        <?php } ?>
+                        <!-- <?php if ( $comments == 'true' && comments_open() && '0' != get_comments_number() ) { ?>
                         <p class="from_the_blog_comments uppercase is-xsmall">
                             <?php
                                 $comments_number = get_comments_number( get_the_ID() );
@@ -282,31 +291,37 @@ while ( $recentPosts->have_posts() ) : $recentPosts->the_post();
                                     number_format_i18n( $comments_number ) )
                             ?>
                         </p>
-                    <?php } ?>
+                        <?php } ?> -->
 
-					<?php if($readmore) { ?>
-						<button href="<?php echo get_the_permalink(); ?>" class="button <?php echo $readmore_color; ?> is-<?php echo $readmore_style; ?> is-<?php echo $readmore_size; ?> mb-0">
-							<?php echo $readmore ;?>
-						</button>
-					<?php } ?>
+                        <?php if($readmore) { ?>
+                        <button href="<?php echo get_the_permalink(); ?>"
+                            class="button <?php echo $readmore_color; ?> is-<?php echo $readmore_style; ?> is-<?php echo $readmore_size; ?> mb-0">
+                            <?php echo $readmore ;?>
+                        </button>
+                        <?php } ?>
 
-					<?php do_action('flatsome_blog_post_after'); ?>
+                        <?php do_action('flatsome_blog_post_after'); ?>
 
-					</div>
-					</div>
-					<?php if(has_post_thumbnail() && ($show_date == 'badge' || $show_date == 'true')) {?>
-					<?php if(!$badge_style) $badge_style = get_theme_mod('blog_badge_style', 'outline'); ?>
-						<div class="badge absolute top post-date badge-<?php echo $badge_style; ?>">
-							<div class="badge-inner">
-								<span class="post-date-day"><?php echo get_the_time('d', get_the_ID()); ?></span><br>
-								<span class="post-date-month is-xsmall"><?php echo get_the_time('M', get_the_ID()); ?></span>
-							</div>
-						</div>
-					<?php } ?>
-				</div>
-				</a>
-			</div>
-		</div>
+                    </div>
+					
+                    <div class="readmore">
+                        <a href="<?php the_permalink() ?>">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+            <?php if(has_post_thumbnail() && ($show_date == 'badge' || $show_date == 'true')) {?>
+            <?php if(!$badge_style) $badge_style = get_theme_mod('blog_badge_style', 'outline'); ?>
+            <div class="badge absolute top rounded post-date badge-<?php echo $badge_style; ?>">
+                <div class="badge-inner color-date">
+                    <span class="post-date-day"><?php echo get_the_time('d', get_the_ID()); ?></span><br>
+                    <span class="post-date-month is-xsmall"><?php echo get_the_time('M', get_the_ID()); ?></span>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+
+    </div>
+</div>
 <?php endwhile;
 wp_reset_query();
 
