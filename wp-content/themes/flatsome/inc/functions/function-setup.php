a49e3149860df099e77d97c62f07a0f4e4b04a4a
TranThiHoaiTrang <68778348+TranThiHoaiTrang@users.noreply.github.com>
2021-12-08 14:46:27 +0700

    fix bug

--- wp-content/themes/flatsome/inc/functions/function-setup.php
+++ wp-content/themes/flatsome/inc/functions/function-setup.php
@@ -90,14 +90,14 @@ function flatsome_widgets_init() {
 	) );
 
 
-	register_sidebar( array(
-		'name'          => __( 'Footer 1', 'flatsome' ),
-		'id'            => 'sidebar-footer-1',
-		'before_widget' => '<div id="%1$s" class="col pb-0 widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<span class="widget-title">',
-		'after_title'   => '</span><div class="is-divider small"></div>',
-	) );
+	// register_sidebar( array(
+	// 	'name'          => __( 'Footer 1', 'flatsome' ),
+	// 	'id'            => 'sidebar-footer-1',
+	// 	'before_widget' => '<div id="%1$s" class="col pb-0 widget %2$s">',
+	// 	'after_widget'  => '</div>',
+	// 	'before_title'  => '<span class="widget-title">',
+	// 	'after_title'   => '</span><div class="is-divider small"></div>',
+	// ) );
 
 	register_sidebar( array(
 		'name'          => __( 'Footer 2', 'flatsome' ),
